e98ccf14a0a467b652a83bbe7bc59ab9a49ed823
Abhinav <akumar261089@gmail.com>
2016-01-13 17:11:24 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,75 @@
 var express = require('express');
 var router = express.Router();
-var db = {};
+//var db = {};
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/:id', function(req, res, next) {
-  str = db[req.param.id] ||{};
-  res.json(str);
+//  str = db[req.param.id] ||{};
+  var query = {};
+  var key = req.params.id;
+  query[key] = {'$exists': true};
+  collection.findOne(query, function (err , doc){
+
+    res.json(doc);
+    return;
+  })
+  return;
 });
 // POST
 router.post('/:id', function(req, res, next) {
-  db[req.param.id] = req.body;
+//  db[req.param.id] = req.body;
+  var obj = {};
+  console.log(req.params.id);
+  obj[req.params.id] = req.body;
+  collection.insert(obj, function (err)
+  {if (err == null)
+    {
+      console.log('insert doc fine');
+    }
+
+  })
   res.json(req.body);
 });
+
+
+
 // UPDATE
 router.put('/:id', function(req, res, next) {
-  db[req.param.id] = req.body;
-  res.json(req.body);
+//  db[req.param.id] = req.body;
+  var query = {};
+  var key = req.params.id;
+  query[key] = {'$exists': true};
+  var new_data ={};
+  new_data[key] = req.body;
+  collection.updateOne (query, {'$set': new_data}, function (err,doc){
+
+    if (err == null ) {
+
+      res.json(req.body);
+    }
+    return;
+  })
+  return;
 });
+
+
 //DELETE
 router.delete('/:id', function(req, res, next) {
-  delete db[req.param.id];
-  res.json({"msg":"successfully deleted "});
+//  delete db[req.param.id];
+  var query = {};
+  var key = req.params.id;
+  query[key] = {'$exists': true};
+  collection.deleteOne(query, function (err,doc){
+    if (err == null){
+      res.json({"msg":"successfully deleted "});
 
+    }
+    return;
+  })
+  return;
 });
 
 
@@ -33,14 +78,14 @@ router.delete('/:id', function(req, res, next) {
 
 module.exports = router;
 
-//var client = require('mongodb').MongoClient;
-//var url = 'mongodb://localhost:27017/test';
-//var collection;
-//client.connect(url,function(err,db){
- // if(err == null)
- // {
-  //  console.log("connection correctly to mongo");
-   // collection = db.collection('test');
- // }
+var client = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/test';
+var collection;
+client.connect(url,function(err,db){
+  if(err == null)
+  {
+    console.log("connection correctly to mongo");
+    collection = db.collection('test');
+  }
 
-//});
+});
